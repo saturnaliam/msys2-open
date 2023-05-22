95aa9ab533c16a535b3a95ad35fbc94fb922a02a
saturnaliam <llcsaturnaliam@gmail.com>
2023-05-22 10:08:05 -0500

    literally all the code

---
+++ README.md
@@ -0,0 +1,3 @@
+# omsys2
+
+i got tired of putting in those command line arguments to open up msys so i just made this
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int main() {
+        system("cls");
+        system("C:/msys64/msys2_shell.cmd -defterm -here -no-start -msys2");
+}
